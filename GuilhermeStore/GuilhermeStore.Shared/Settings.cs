28c5ab06a989a6316e79a74690ed65619f616b39
Guilherme Ramos Mendes <guilherme.mendes@interplayers.com.br>
2019-04-09 21:31:39 -0300

    Ajuste DataContext

--- GuilhermeStore/GuilhermeStore.Shared/Settings.cs
+++ GuilhermeStore/GuilhermeStore.Shared/Settings.cs
@@ -4,7 +4,7 @@ namespace GuilhermeStore.Shared
 {
     public static class Settings
     {
-        public static string ConnectionString = null;
+        public static string ConnectionString;
         
         // = @"Server=(LocalDb)\MSSQLLocalDB;Database=GuilhermeStore;User ID=guilherme;Password=340$Uuxwp7Mcxo7Khy";
 
