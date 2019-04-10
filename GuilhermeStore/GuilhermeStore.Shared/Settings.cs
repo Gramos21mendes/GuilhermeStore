a6ce326d997aef04171779e5e2fd50f3ea640c27
Guilherme Ramos Mendes <guilherme.mendes@interplayers.com.br>
2019-04-09 21:50:30 -0300

    Configurações

--- GuilhermeStore/GuilhermeStore.Shared/Settings.cs
+++ GuilhermeStore/GuilhermeStore.Shared/Settings.cs
@@ -5,8 +5,9 @@ namespace GuilhermeStore.Shared
     public static class Settings
     {
         public static string ConnectionString;
-        
-        // = @"Server=(LocalDb)\MSSQLLocalDB;Database=GuilhermeStore;User ID=guilherme;Password=340$Uuxwp7Mcxo7Khy";
+
+        //Local Connection   
+        // = @"Server=(LocalDb)\\MSSQLLocalDB;Database=GuilhermeStore;User ID=guilherme;Password=340$Uuxwp7Mcxo7Khy";
 
         //340$Uuxwp7Mcxo7Khy
     }
